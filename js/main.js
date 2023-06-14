7f67b353682e4963fccb7b1bbe68bf135badd170
Clayton Critchfield <clayton.critchfield@gmail.com>
2023-06-14 19:39:17 +0000

    added value to apiKey variable, added functions to convert Celsius to Kelvin and Fahrenheit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 //VARIABLES
-let button = document.getElementById("button");
+let apiKey = c097438b59289f230ebfc5b099c90073;
+let button = document.getElementById('button');
 let zipcode;
+let city;
 let celsius;
 let fahrenheit;
+let kelvin;
+let icon;
+let weather; 
 
-button.addEventListener('click', checkZipcode);
+//Use AXIOS to get data
+
+
+//EVENT LISTENERS
+button.addEventListener('click', checkZipcode); //This event listener also needs to update page by GET weather using AXIOS, will need to update upon API fetch
 
 
 
@@ -14,8 +23,18 @@ function checkZipcode() {
     if (zipcode.length !== 5) {
         alert('Error: please enter valid zipcode')
     } else {
-        console.log('valid zip code')
+        console.log('valid zip code, now GET data')
         return zipcode;
     }
 };
+
+function tempToKelvin(celsius) {
+    kelvin = celsius + 273.15;
+    return kelvin;
+};
+
+function tempToFahrenheit(celsius) {
+    fahrenheit = (celsius + 9 / 5) + 32;
+    return fahrenheit; 
+}
 
