7ba6ec6a6c770057e7b143dab82694d66acbdfd8
Clayton Critchfield <clayton.critchfield@gmail.com>
2023-06-14 19:00:41 +0000

    created button and zipcode variables retrieved from DOM, added event listener to zipcode to validate if correct length

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+//VARIABLES
+let button = document.getElementById("button");
+let zipcode = '';
 
+button.addEventListener('click', checkZipcode);
+
+
+
+//FUNCTIONS 
+function checkZipcode() {
+    let zipcode = document.getElementById("zipcode");
+    if (zipcode !== 5) {
+        alert('Error: please enter valid zipcode')
+    } else {
+        console.log('valid zip code')
+    }
+};
