ed239f6ce27a29c57f3b751b4ce41f2a0fc8fc5b
Clayton Critchfield <clayton.critchfield@gmail.com>
2023-06-15 17:09:46 +0000

    redid temperature conversion function since default data is in Kelvin

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //VARIABLES
-let apiKey = c097438b59289f230ebfc5b099c90073;
+let apiKey = 'c097438b59289f230ebfc5b099c90073';
 let button = document.getElementById('button');
-let zipcode = document.getElementById("zipcode");
+let zipcode;
 let cityDisplay = document.getElementById("cityDisplay");
 let celsius = document.getElementById("celsius");
 let fahrenheit = document.getElementById("fahrenheit");
@@ -9,16 +9,6 @@ let kelvin = document.getElementById("kelvin");
 let icon = document.getElementById("iconDisplay");
 const apiURL = "https://api.openweathermap.org";
 let apipath = "/data/2.5/weather";
-
-//Use AXIOS to get data
-import axios from 'axios';
-const axios = require('axios'.default);
-
-// Make a request for a user with a given ID
-function getWeatherData() {
-  let weatherData = {};
-}
-
 
 //EVENT LISTENERS
 button.addEventListener('click', checkZipcode); //This event listener also needs to update page by GET weather using AXIOS, will need to update upon API fetch
@@ -29,15 +19,42 @@ button.addEventListener('click', checkZipcode); //This event listener also needs
 function checkZipcode() {
     let zipcode = document.getElementById("zipcode").value;
     if (zipcode.length !== 5) {
-        alert('Error: please enter valid zipcode')
+        alert('Error: please enter valid zipcode');
     } else {
         console.log('valid zip code, now GET data')
         return zipcode;
     }
 };
 
-function tempToKelvin(celsius) {
-    kelvin = celsius + 273.15;
+//Use AXIOS to get data
+import axios from 'axios';
+const axios = require('axios'.default);
+
+// Make a request from the weather API using base url, zipcode, and apikey
+function getWeatherData() {
+  let weatherData = {};
+
+  try {
+    let option = {
+      baseURL: apiURL,
+      params: {
+        zip: zipcode;
+        appid: apiKey
+      }
+    };
+    weatherData = await axios.get(apipath, options)
+  }
+
+  catch (error) {
+    //returns an error if there is one
+    console.log("Error in the API call, need to debug");
+  };
+  return weatherData;
+}
+
+//CONVERSION FUNCTIONS
+function tempToCelsius(kelvin) {
+    celsius = weatherData.temperature - 273.15;
     return kelvin;
 };
 
